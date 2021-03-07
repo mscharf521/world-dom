4b6521392c527f5fd2331da6819c91c588806707
Matthew Scharf <mscharf521@gmail.com>
2021-03-07 09:42:43 -0600

    Add kofi button

--- src/StartingPage.js
+++ src/StartingPage.js
@@ -35,5 +35,7 @@ export default function StartingPage(props) {
         color="primary"
         onClick={props.OnSubmit}>JOIN</Button>
     </form>
+
+    <a className='kofi-btn' href='https://ko-fi.com/Z8Z73V4Y6' target='_blank' rel="noopener noreferrer"><img height='36' style={{border:'0px', height:'36px'}} src='https://cdn.ko-fi.com/cdn/kofi1.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
   </div>
 }
